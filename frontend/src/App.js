a28ed8f5e6363e34470062621f914b6f371dabc8
Ararine <younadme0112@gmail.com>
2026-03-13 19:10:19 +0900

    fourth commit

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -7,10 +7,10 @@ function App() {
     <>
       <div className="App">
         <Button variant="text" color="secondary">
-          Hello World...
+          Hello World
         </Button>
         <Button variant="contained" color="success">
-          Hello World
+          Hello World...
         </Button>
         <Button variant="outlined" color="error">
           Hello World
